dfa39df0583112b930df83917ce339bcfa3d32e8
gaurav singh <chauhangaurav9090@gmail.com>
2021-05-07 11:48:47 +0530

    add

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 8000;
 const mongoose = require('mongoose');
 const bodyparser = require("body-parser");
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true,useUnifiedTopology: true});
 
 const contactSchema = new mongoose.Schema({
     name: String,
